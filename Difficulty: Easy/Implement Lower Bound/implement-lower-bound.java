f77b6087f37c5a3decc5252ad87bd9bc1ab0069b
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-10-14 01:06:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -4,12 +4,12 @@ class Solution {
         int n = arr.length;
         int st = 0;
         int end = n-1;
-        int ans = n;
+        int ans = n;   // default: agar sab chhote hon, to answer = n (insert at end)
         while(st <= end){
             int mid = st + (end - st) / 2;
             if(arr[mid] >= target){
                 ans = mid;
-                end = end - 1;
+                end = mid - 1;
             }
             else{
                 st = mid + 1;
