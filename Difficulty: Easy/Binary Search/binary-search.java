5390991b2ee5779c924c1845d765507270c5ec5e
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-11-02 17:04:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -4,20 +4,20 @@ class Solution {
         int n = arr.length;
         int st = 0;
         int end = n-1;
-        int ans = -1;
+        int idx = -1;
         while(st <= end){
             int mid = st + (end - st) / 2;
-            if(k > arr[mid]){
-                st = mid + 1;
-            }
-            else if(k < arr[mid]){
+            if(arr[mid] == k){
+                idx = mid;
                 end = mid - 1;
             }
-            else{    //arr[mid] == k
-                ans = mid;
+            else if(k > arr[mid]){
+                st = mid + 1;
+            }
+            else{
                 end = mid - 1;
             }
         }
-        return ans;
+        return idx;
     }
 }
