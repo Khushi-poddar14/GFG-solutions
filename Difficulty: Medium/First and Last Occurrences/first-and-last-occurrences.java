58bec6f9fa61b2177c974607de75edd7850055ee
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2025-11-02 17:56:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -4,16 +4,16 @@ class GFG {
     ArrayList<Integer> find(int arr[], int x) {
         // code here
         ArrayList<Integer> nums = new ArrayList<>();
-        int n = arr.length;
-        int st = 0;
-        int end = n - 1;
         int first = -1;
         int last = -1;
+        int st = 0;
+        int n = arr.length;
+        int end = n-1;
         while(st <= end){
             int mid = st + (end - st) / 2;
-            if(arr[mid]==x){
+            if(arr[mid] == x){
                 first = mid;
-                end = mid -1;
+                end = mid - 1;
             }
             else if(x > arr[mid]){
                 st = mid + 1;
@@ -25,10 +25,10 @@ class GFG {
         st = 0;
         end = n-1;
         while(st <= end){
-            int mid = st + (end - st)/2;
+            int mid = st + (end - st) / 2;
             if(arr[mid] == x){
                 last = mid;
-                st = mid + 1;
+                st = mid+1;
             }
             else if(x > arr[mid]){
                 st = mid + 1;
