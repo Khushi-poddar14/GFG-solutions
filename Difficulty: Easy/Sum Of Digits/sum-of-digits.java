7fe1de77fac04ccd11f5abed5c330b4d4d12f393
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-02-05 16:56:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum Of Digits/sum-of-digits.java
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.java
@@ -1,16 +1,10 @@
 class Solution {
-    public static int sum(int n){
-        int x = n;
-        int s = 0;
-        while(n!=0){
-            x = n%10;
-            s+=x;
-            n = n/10;
-        }
-        return s;
+    static int sod(int n){
+        if(n>=0 && n <= 9) return n;
+        return n%10 + sod(n/10);
     }
     static int sumOfDigits(int n) {
         // code here
-        return sum(n);
+        return sod(n);
     }
 }
