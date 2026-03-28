71068a629e15c9e2cab05be30164be60f4e020f3
Khushi-poddar14 <156164281+Khushi-poddar14@users.noreply.github.com>
2026-03-29 00:56:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -1,21 +1,18 @@
-// User function Template for Java
-
-class GFG {
+class Solution {
     ArrayList<Integer> find(int arr[], int x) {
         // code here
-        ArrayList<Integer> nums = new ArrayList<>();
-        int first = -1;
-        int last = -1;
-        int st = 0;
+        ArrayList<Integer> a = new ArrayList<>();
         int n = arr.length;
+        int st = 0;
         int end = n-1;
+        int f = -1;
         while(st <= end){
             int mid = st + (end - st) / 2;
             if(arr[mid] == x){
-                first = mid;
+                f = mid;
                 end = mid - 1;
             }
-            else if(x > arr[mid]){
+            else if(arr[mid] < x){
                 st = mid + 1;
             }
             else{
@@ -24,21 +21,20 @@ class GFG {
         }
         st = 0;
         end = n-1;
+        a.add(f);
+        int l = -1;
         while(st <= end){
             int mid = st + (end - st) / 2;
             if(arr[mid] == x){
-                last = mid;
-                st = mid+1;
-            }
-            else if(x > arr[mid]){
+                l = mid;
                 st = mid + 1;
             }
+            else if(arr[mid] < x) st = mid + 1;
             else{
                 end = mid - 1;
             }
         }
-        nums.add(first);
-        nums.add(last);
-        return nums;
+        a.add(l);
+        return a;
     }
 }
